21adb44ecc3cf534e2a887c7c1b255cd649138c3
Samuel <samuelldmj5@gmail.com>
2024-06-19 15:37:12 +0100

    intro to class

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
 
+/* 
+i moved out of the current directory to the parent directory, this is so because the file is being loaded in the pulic directory,
+inside the index file */
+require_once "../Transaction.php";
 
-echo "Hello world";
+$transaction = (new Transaction(100, 'coffee'))->
+    // $transaction->amount = 25;
+    addTax(8)->applyDiscount(10)->getAmount() . "<br>";
+var_dump($transaction);
+
+$transaction1 = (new Transaction(20, 'tea'))->addTax(1)->getAmount();
+var_dump($transaction1);
