5c7ed88ba21c5c9e0a4301f66e8a53418e1ef1c1
samuelldmj <samuelldmj5@gmail.com>
2025-01-06 19:11:11 +0100

    ht access file added

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,7 +12,7 @@ class UserModel extends Model
     public function create(string $email, string $name, bool $is_active = true): int
     {
 
-        $Stmt = $this->db->prepare('INSERT INTO users (email, full_name, is_active, created_at) VALUES (?, ?, ?, NOW()) ');
+        $Stmt = $this->db->prepare('INSERT INTO oop_users (email, full_name, is_active, created_at) VALUES (?, ?, ?, NOW()) ');
         $Stmt->execute([$email, $name, $is_active]);
         return  (int) $this->db->lastInsertId();
     }
