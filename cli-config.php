8c0e2e9407db1a4e12e85cdb7a1d6ee9d0d6a96b
samuelldmj <samuelldmj5@gmail.com>
2025-02-05 13:02:15 +0100

    learnt how to use doctrine migration library to create DB schema

---
+++ cli-config.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = new PhpFile('migrations.php'); // Or use one of the Doctrine\Migrations\Configuration\Configuration\* loaders
+
+$params = [
+    'dbname' => $_ENV["DB_DATABASE"],
+    'user' => $_ENV['DB_USER'],
+    'password' => '',
+    'host' => $_ENV['DB_HOST'],
+    'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
+];
+
+$paths = [__DIR__ . '/app/Entity'];
+$isDevMode = true;
+
+$ORMConfig = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
+$connection = DriverManager::getConnection($params);
+
+$entityManager = new EntityManager($connection, $ORMConfig);
+
+return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
